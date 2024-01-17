66102a8ae9401e12e158fd4c66faa315e4598d51
ivanpereyra10 <ivanpereyra67@gmail.com>
2024-01-16 23:58:09 -0300

    Arreglos

--- ecommerce/src/conmponents/Item/Item.jsx
+++ ecommerce/src/conmponents/Item/Item.jsx
@@ -2,7 +2,7 @@ const Item = ({ id, name, img, category, price }) => {
     return (
         <article>
             <h3>{name}</h3>
-            <img src={img}/>
+            <img src={img} style={{ width: 200}}/>
             <p> Categoria:{category}</p>
             <h4>${price}</h4>
         </article>
